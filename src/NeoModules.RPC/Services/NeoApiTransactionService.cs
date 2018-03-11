c1cdb51bf5ee14c4c5c647699d8e4cf0cea41bab
BrunoFreitasgit <bruno_freitas_2015@outlook.com>
2018-03-11 18:04:32 +0000

    missing merge stuff

--- src/NeoModules.RPC/Services/NeoApiTransactionService.cs
+++ src/NeoModules.RPC/Services/NeoApiTransactionService.cs
@@ -7,16 +7,19 @@ namespace NeoModules.RPC.Services
     {
         public NeoApiTransactionService(IClient client) : base(client)
         {
+            GetApplicationLog = new NeoGetApplicationLog(client);
             GetRawTransaction = new NeoGetRawTransaction(client);
             GetRawTransactionSerialized = new NeoGetRawTransactionSerialized(client);
             SendRawTransaction = new NeoSendRawTransaction(client);
             GetTransactionOutput = new NeoGetTransactionOutput(client);
+            SendAssets = new NeoSendAssets(client);
         }
 
+        public NeoGetApplicationLog GetApplicationLog { get; private set; }
         public NeoGetRawTransaction GetRawTransaction { get; private set; }
         public NeoGetRawTransactionSerialized GetRawTransactionSerialized { get; private set; }
         public NeoSendRawTransaction SendRawTransaction { get; private set; }
         public NeoGetTransactionOutput GetTransactionOutput { get; private set; }
-       
+        public NeoSendAssets SendAssets { get; private set; }
     }
 }
