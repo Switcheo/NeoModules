627eebe53fab46877e1f0e1d0ec64e344b1624a0
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-04-16 15:41:28 +0100

    Added "Create Account" to Xamarin sample

--- samples/NeoModulesXF/NeoModulesXF/NeoModulesXF/ViewModels/WalletViewModel.cs
+++ samples/NeoModulesXF/NeoModulesXF/NeoModulesXF/ViewModels/WalletViewModel.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using NeoModules.NEP6;
 using NeoModules.NEP6.Models;
 using PropertyChanged;
+using Xamarin.Forms;
 
 namespace NeoModulesXF.ViewModels
 {
@@ -13,10 +14,25 @@ namespace NeoModulesXF.ViewModels
         public Wallet TestWallet { get; set; }
         public WalletManager TestWalletManager { get; set; }
 
+        [DependsOn("TestWalletManager.Accounts")]
+        public ObservableCollection<Account> Accounts { get; set; }
+
+        public ICommand CreateNewAccountCommand => new Command(CreateNewAccountExecute);
+
+        private int count = 0;
+
         public WalletViewModel()
         {
             TestWallet = new Wallet("test wallet");
             TestWalletManager = new WalletManager(TestWallet);
+            Accounts = new ObservableCollection<Account>(TestWallet.Accounts);
+        }
+
+        private void CreateNewAccountExecute()
+        {
+            var account = TestWalletManager.CreateAccount("TestAccount" + count);
+            Accounts.Add(account);
+            count++;
         }
     }
 }
