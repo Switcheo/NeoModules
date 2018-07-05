c981baac0746945991cc4715a61397465bc8ccb9
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-07-05 15:56:42 +0100

    Several fixes to Unity sample

--- samples/NeoModulesUnity/Assets/NeoModulesDemo.cs
+++ samples/NeoModulesUnity/Assets/NeoModulesDemo.cs
@@ -1,34 +1,57 @@
 ﻿using System;
+using System.Threading.Tasks;
 using NeoModules.Core;
 using NeoModules.JsonRpc.Client;
 using NeoModules.NEP6;
+using NeoModules.NEP6.Transactions;
 using NeoModules.Rest.Services;
 using NeoModules.RPC;
+using NeoModules.RPC.DTOs;
 using UnityEngine;
 
 public class NeoModulesDemo : MonoBehaviour
 {
 
+    readonly string URI = "https://seed1.spotcoin.com:10332";
+    private readonly string contractHash = "ed07cffad18f1308db51920d99a2af60ac66a7b3";
+
     // Use this for initialization
     async void Start()
     {
-        // Create rpc client and service
-        var neoRpcClient = new RpcClient(new Uri("http://seed1.cityofzion.io:8080"));
+        try
+        {
+            var invoke = await InvokeContract();
+            Debug.Log(invoke.Stack[0].Value.ToString());
+            var contract = await CallContract();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+    }
+
+    async Task<Invoke> InvokeContract()
+    {
+        var neoRpcClient = new RpcClient(new Uri(URI));
         var neoRpcService = new NeoApiService(neoRpcClient);
 
         // Invoke script
-        var contractScriptHash = UInt160.Parse("ed07cffad18f1308db51920d99a2af60ac66a7b3").ToArray();
-        var script = NeoModules.NEP6.Utils.GenerateScript(contractScriptHash, "getAddressFromMailbox",
+        var contractScriptHash = UInt160.Parse(contractHash).ToArray();
+        var script = Utils.GenerateScript(contractScriptHash, "getAddressFromMailbox",
             new object[] { "teste" });
-        var response = await neoRpcService.Contracts.InvokeScript.SendRequestAsync(script.ToHexString());
+        return await neoRpcService.Contracts.InvokeScript.SendRequestAsync(script.ToHexString());
+    }
 
-
+    async Task<SignedTransaction> CallContract()
+    {
         // Wallet manager and contract calls
+        var neoRpcClient = new RpcClient(new Uri(URI));
         var walletManager = new WalletManager(new NeoScanRestService(NeoScanNet.MainNet), neoRpcClient);
-
+        var contractScriptHash = UInt160.Parse(contractHash).ToArray();
         var importedAccount = walletManager.CreateAccount("unity account");
         var accountSigner = importedAccount.TransactionManager as AccountSignerTransactionManager;
-        var response2 = await accountSigner.CallContract(contractScriptHash, "registerMailbox",
+        return await accountSigner.CallContract(contractScriptHash, "registerMailbox",
             new object[] { importedAccount.Address.ToArray(), "unitytest" });
     }
 
