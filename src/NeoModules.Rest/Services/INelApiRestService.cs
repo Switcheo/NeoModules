c368d696df9802fe0c4218a86a39a5cf06ef95c4
Bruno Freitas <bruno_freitas_2015@outlook.com>
2018-09-02 22:30:15 +0100

    Start of Nel Api rest service implementation

--- src/NeoModules.Rest/Services/INelApiRestService.cs
+++ src/NeoModules.Rest/Services/INelApiRestService.cs
@@ -1,7 +1,22 @@
-﻿namespace NeoModules.Rest.Services
+﻿using System.Threading.Tasks;
+
+namespace NeoModules.Rest.Services
 {
-    public interface INelApiRestService
+    public interface INelApiRestService //todo: DTOs
     {
-        
+        Task<long> GetBlockCount(); //GetHeight
+        Task<string> GetBlock(int height);
+        Task<string> GetRawTransaction(string txHash);
+        Task<string> GetAsset(string assetHash);
+        Task<string> GetFullLog(string txHash);
+        Task<string> GetNotify(string txHash);
+        Task<string> GetUtxo(string address);
+        Task<string> GetUtxoCount(string address);
+        Task<string> GetUtxosToPay(string address, string txHash, decimal firstValue, decimal secondValue);
+        Task<string> GetBalance(string address);
+        Task<string> GetBlocks(int maxHeigth, int minHeight);
+        Task<string>
+            GetRawTransactions(int maxHeight, int minHeight,
+                string type); //txType:EnrollmentTransaction,PublishTransaction,InvocationTransaction,ClaimTransaction,MinerTransaction,RegisterTransaction,IssueTransaction,ContractTransaction
     }
 }
---
+++ src/NeoModules.Rest/Services/NelApiRestService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoModules.Rest.Services
+{
+    public class NelApiRestService:INelApiRestService
+    {
+        private static readonly string nelScanMainNetUrl = "https://api.nel.group/api/testnet";
+        private static readonly string nelScaTestNetUrl = "https://api.nel.group/api/mainnet";
+
+        private static readonly string getBalance = "get_balance/";
+        private static readonly string getClaimed = "get_claimed/";
+        private static readonly string getClaimable = "get_claimable/";
+        private static readonly string getUnclaimed = "get_unclaimed/";
+        private static readonly string getAllNodes = "get_all_nodes/";
+
+        public Task<long> GetBlockCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetBlock(int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRawTransaction(string txHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetAsset(string assetHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetFullLog(string txHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNotify(string txHash)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUtxo(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUtxoCount(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUtxosToPay(string address, string txHash, decimal firstValue, decimal secondValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetBalance(string address)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetBlocks(int maxHeigth, int minHeight)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetRawTransactions(int maxHeight, int minHeight, string type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
